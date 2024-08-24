e02cf59b610d81b9aa6ec63c9a6afe12ad209721
EmanRiaz <emanriaz504@gmail.com>
2024-08-24 22:47:29 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+
+       // 1. Display Age in Alert
+        function showAge() {
+            let age = 15; // Your age
+            alert("I am " + age + " years old");
+        }
+
+        // 2. Track Page Visits
+        let visitCount = 0;
+        function trackVisits() {
+            visitCount++;
+            alert("You have visited this site " + visitCount + " time(s)");
+        }
+
+        // 3. Show Birth Year and Data Type
+        function showBirthYear() {
+            let birthYear = 1990; // Your birth year
+            alert("My birth year is " + birthYear + "\nData type of my declared variable is " + typeof birthYear);
+        }
+
+        // 4. Online Clothing Store Order
+        function showOrderConfirmation() {
+            let visitorName = "John Doe";
+            let productTitle = "T-shirt";
+            let quantity = 5;
+            alert(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+        }
+    
